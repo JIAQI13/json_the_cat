6b230ed6273dc47144c98be9a7c6d60ef2b20061
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-06 23:21:00 +0000

    test finished

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,14 @@
 
 const request = require('request');
-let name = process.argv[2];
-request(`https://pi.thecatapi.com/v1/breeds/search?q=${name}`, (error, response, body) => {
-  if (!error) {console.log('error:', error);}
-   // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  //console.log(typeof body)
-  const data = JSON.parse(body);
-  if (data.length === 0){
-      console.log('not found');
-  }else{
-      console.log(data[0].description);
-  }
-  //console.log(typeof data);
-
-});
+const fetchBreedDescription = (breedName, callback) =>{
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error,null);
+    } else {
+      const data = JSON.parse(body);
+      //data.length === 0 ? console.log('not found'):console.log(data[0].description);
+      callback(null,data[0].description);
+    }
+  });
+};
+module.exports = {fetchBreedDescription};
