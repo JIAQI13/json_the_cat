a5b9219740c52f2d12b17bcb0da7f18b0843c186
JIAQI13 <jiaqi11@ualberta.ca>
2021-05-06 23:28:40 +0000

    one liner wins

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,4 @@
 
 const request = require('request');
-const fetchBreedDescription = (breedName, callback) =>{
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    if (error) {
-      callback(error,null);
-    } else {
-      const data = JSON.parse(body);
-      //data.length === 0 ? console.log('not found'):console.log(data[0].description);
-      callback(null,data[0].description);
-    }
-  });
-};
+const fetchBreedDescription = (breedName, callback) => request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {error ?  callback(error,null) : callback(null,JSON.parse(body)[0].description);});
 module.exports = {fetchBreedDescription};
